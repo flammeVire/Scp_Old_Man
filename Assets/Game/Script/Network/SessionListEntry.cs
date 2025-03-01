10f5eaf3f82d71d384747be1760d93f3deab61f7
flammeVire <noahcoevoet@gmail.com>
2025-03-01 17:58:53 +0100

    lobby multi

---
+++ Assets/Game/Script/Network/SessionListEntry.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Fusion;
+using UnityEngine.UI;
+
+
+public class SessionListEntry : MonoBehaviour
+{
+    public TextMeshProUGUI roomName, PlayerCount;
+    public Button JoinButton;
+
+    public void JoinRoom()
+    {
+        NetworkManager.runnerInstance.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Shared,
+            SessionName = roomName.text,
+        });
+        Debug.Log(roomName.text);
+    }
+}
