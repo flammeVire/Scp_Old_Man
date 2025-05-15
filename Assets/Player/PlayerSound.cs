a0d83732604f5ff4525b6027ffdab0d4f4bd184b
MatthieuTardy <158582387+MatthieuTardy@users.noreply.github.com>
2025-05-15 16:22:05 +0200

    Fix n Sound

---
+++ Assets/Player/PlayerSound.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using FMODUnity;
+
+public class PlayerSound : NetworkBehaviour
+{
+    [Header("SFX")]
+    public StudioEventEmitter walk;
+    public StudioEventEmitter crouch;
+    public StudioEventEmitter jump;
+    public StudioEventEmitter run;
+
+    // Start is called before the first frame update
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void Rpc_Walk(bool isWalking)
+    {
+        if (walk != null)
+        {
+            if (isWalking == true)
+            {
+                walk.Play();
+
+            }
+            else 
+            {
+                walk.Stop();
+
+            }
+        }
+    }
+
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void Rpc_Run(bool isRunning)
+    {
+        if (walk != null)
+        {
+            if (isRunning == true)
+            {
+                run.Play();
+
+            }
+            else
+            {
+                run.Stop();
+
+            }
+        }
+    }
+}
