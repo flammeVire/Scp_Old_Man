7490b2cfeecb2023f5f672a7444271c93ee62467
flammeVire <noahcoevoet@gmail.com>
2025-05-18 18:36:40 +0200

    Sas prefab fix

--- Assets/Game/Script/Iterraction/Sas_Door.cs
+++ Assets/Game/Script/Iterraction/Sas_Door.cs
@@ -9,7 +9,22 @@ public class Sas_Door : NetworkBehaviour
     public NetworkObject Door;
     public Transform Spawn;
 
+    [SerializeField] Transform Button1;
+    [SerializeField] BoxCollider Collider1;
+    [SerializeField] Transform Button2;
+    [SerializeField] BoxCollider Collider2;
+
+
     public float Delay;
+
+    public void OnValidate()
+    {
+        Collider1.center = Button1.localPosition;
+        Collider1.size = Button1.localScale;
+        Collider2.center = Button2.localPosition;
+        Collider2.size = Button2.localScale;
+    }
+
 
     [Rpc(RpcSources.All,RpcTargets.All)]
     public void Rpc_OpeningSas(NetworkBool IsOpen)
