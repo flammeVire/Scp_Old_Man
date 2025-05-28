f110c66df04c74cab421b85c88d532a3a82edae1
MatthieuTardy <158582387+MatthieuTardy@users.noreply.github.com>
2025-05-28 03:10:00 +0200

    Menu fin

---
+++ Assets/Game/Script/Menu fin.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using UnityEngine;
+
+public class Menufin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
